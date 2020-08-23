eb136ce285c160d4a1584487eb95b653671474d3
Stepan Bakshayev <step2back@gmail.com>
2020-08-23 17:08:11 +0700

    implement path tracing of elements

--- saganineeleven/stringify.py
+++ saganineeleven/stringify.py
@@ -15,7 +15,8 @@
 # along with saganineeleven.  If not, see <https://www.gnu.org/licenses/>.
 from xml.etree import ElementTree
 from enum import Enum
-
+from dataclasses import dataclass, field, make_dataclass
+from collections import deque, Counter
 
 Token = Enum('Event', 'text terminal', module=__name__)
 
@@ -37,26 +38,63 @@ Token = Enum('Event', 'text terminal', module=__name__)
 # 		return (Token.text, self.chunk),
 
 
+@dataclass(frozen=True)
+class Element:
+	path: str
+	offset: int
+	length: int
+
+
+class elementstr(str):
+	pass
+
+
 def stringify(
 	file,
 	Lexer
 ) -> str:
 	text = []
 	lexer = Lexer()
+	route = deque()
 	# XXX: this is not pull parser. Consider in the future the possibility of using XMLPullParser.
-	for (event, element) in ElementTree.iterparse(file, events=('end',)):
-		tree = element.iter()
-		# pull root. It is node by self.
-		next(tree)
-		# The invariant is childfree nodes with text.
-		if next(tree, None) is not None:
-			continue
-		if element.text is None:
-			continue
-		lexer.feed(element.text)
-		for event, elem in lexer.read_events():
-			print(event, elem)
-		text.append(element.text or '')
+	for (event, element) in ElementTree.iterparse(file, events=('start', 'end',)):
+		if event == 'start':
+			tag = element.tag
+			if route:
+				route[-1][1][tag] += 1
+			route.append((tag, Counter()))
+
+		elif event == 'end':
+			route.pop()
+
+			# The invariant is childfree nodes with text.
+			# XXX: this is ugly way for checking children.
+			tree = element.iter()
+			# pull root. It is node by self.
+			next(tree)
+			if next(tree, None) is not None:
+				continue
+
+			# shortcut
+			if element.text is None:
+				continue
+
+			chunk = elementstr(element.text)
+			path = deque()
+			element_tag = element.tag
+			for r in reversed(route):
+				path.appendleft(f'{element_tag}[{r[1][element_tag]}]')
+				element_tag = r[0]
+			path.appendleft(f'{element_tag}[1]')
+			chunk.elements = Element('/'.join(path), 0, len(chunk)),
+
+			lexer.feed(chunk)
+			for event, elem in lexer.read_events():
+				print(event, elem)
+			text.append(element.text or '')
+
+		else:
+			raise RuntimeError(f'Unsupported event type {event} from ElementTree.iterparse().')
 
 	lexer.close()
 	for event, elem in lexer.read_events():
