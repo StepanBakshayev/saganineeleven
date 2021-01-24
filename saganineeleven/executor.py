d211f401b9867cc2cc5664540a1c2b8995920076
Stepan Bakshayev <step2back@gmail.com>
2021-01-24 18:16:34 +0700

    prepare for development loop support

--- saganineeleven/executor.py
+++ saganineeleven/executor.py
@@ -191,9 +191,19 @@ def delineate_boundaries(tree_root: Element, line: Line) -> Mapping[Index, Bound
 	return registry
 
 
-def fake_enforce(source: Element, tape: Line, boundaries: Mapping[Index, Boundary]) -> TreeBuilder:
-	builder = TreeBuilder(source)
+@dataclass(frozen=True)
+class ElementPosition:
+	path: Path
+	index: int
 
+
+@dataclass(frozen=True)
+class LoopBody:
+	start: ElementPosition
+	stop: ElementPosition
+
+
+def fake_enforce(source: Element, tape: Line, boundaries: Mapping[Index, Boundary]) -> TreeBuilder:
 	def iterate_boundaries(boundaries, start, stop):
 		for index in range(start, stop):
 			yield index, boundaries[index]
@@ -211,31 +221,41 @@ def fake_enforce(source: Element, tape: Line, boundaries: Mapping[Index, Boundar
 			yield up.opening, len(down.gap.branch) + 1
 			up = down
 
-	previous_path = ()
-	previous_index = min(boundaries) - 1
+	builder = TreeBuilder(source)
+
+	previous_present = ElementPosition(path=(), index=min(boundaries)-1)
+	previous_discarded = None
+	loop_body = None
 	last_discard = False
 	# boundaries are used to skip holes in tree in climbing up in tree and moving forward on tape.
 	for pointer, text in tape:
+		if loop_body and pointer.index > loop_body.stop.index:
+			loop_body = None
+		if previous_discarded and pointer.index < previous_discarded.index:
+			loop_body = LoopBody(ElementPosition(pointer.path, pointer.index), previous_discarded)
+			previous_discarded = None
+
 		# discard
 		if not pointer.is_constant and not text:
+			previous_discarded = ElementPosition(pointer.path, pointer.index)
 			continue
 
 		# calculate real distance movement from previous position.
-		if previous_index + 1 != pointer.index:
-			debug('discard', pointer, text, previous_path, previous_index)
+		if previous_present.index + 1 != pointer.index:
+			debug('discard', pointer, text, previous_present.path, previous_present.index)
 			last_discard = True
 
 		debug(pointer, text)
 		debug(last_discard)
 		# build
 		# XXX: ignore cycles for awhile.
-		if previous_path != pointer.path:
+		if previous_present.path != pointer.path:
 			routes = ()
-			level = previous_path
+			level = previous_present.path
 
-			debug(previous_index)
+			debug(previous_present.index)
 
-			next_to_previous_index = previous_index + 1
+			next_to_previous_index = previous_present.index + 1
 			if next_to_previous_index in boundaries:
 				previous_boundary = boundaries[next_to_previous_index]
 				routes += previous_boundary.ending + (previous_boundary.gap,)
@@ -313,29 +333,29 @@ def fake_enforce(source: Element, tape: Line, boundaries: Mapping[Index, Boundar
 				for element in current_element:
 					if element.tag.endswith('}t'):
 						break
-				if previous_path != pointer.path:
+				if previous_present.path != pointer.path:
 					element.text = text
 				else:
 					element.text += text
 				debug('run', element.text)
 			elif current_element.tag.endswith('}textpath'):
-				if previous_path != pointer.path:
+				if previous_present.path != pointer.path:
 					current_element.set('string', text)
 				else:
-					current_element.set('string', current_element.get('string', '')+text)
+					current_element.set('string', current_element.get('string')+text)
 				debug('textpath', current_element.attrib)
 			else:
 				raise  NotImplementedError(repr(current_element))
 			print('set_text')
 
-		previous_path = pointer.path
-		previous_index = pointer.index
+		previous_present = ElementPosition(pointer.path, pointer.index)
+		previous_discarded = None
 		last_discard = False
 		print()
 
 	routes = ()
-	if previous_index + 1 in boundaries:
-		routes += boundaries[previous_index+1].ending
+	if previous_present.index + 1 in boundaries:
+		routes += boundaries[previous_present.index+1].ending
 	# 1) continue closing
 	# copy current
 	last_index = max(boundaries)
@@ -369,195 +389,3 @@ def make_x(root: Element, path: Path) -> List[str]:
 		chunks.append(f'{tag}[{count}]')
 		parent = child
 	return chunks
-
-
-# # plane:
-# # - text
-# # - element
-# # element:
-# # - direction
-# # - action
-#
-#
-#
-#
-# Operation = Enum('Operation', 'copy set_text', module=__name__)
-#
-#
-# ElementOperation = Enum('ElementOperation', 'copy set_text none', module=__name__)
-#
-#
-# def get_operation(pointer: ElementPointer, text: str) -> ElementOperation:
-# 	if pointer.is_constant:
-# 		assert pointer.offset == 0, (pointer, text)
-# 		assert pointer.length == len(text)
-# 		return ElementOperation.copy
-# 	elif text:
-# 		assert pointer.offset < pointer.representation_length, (pointer, text)
-# 		assert pointer.length, (pointer, text)
-# 		return ElementOperation.set_text
-# 	return ElementOperation.none
-#
-#
-
-#
-#
-# def decode(tree_root: Element, tape: Iterator[Tuple[ElementPointer, str]]) -> Iterator[Union[Dict[Literal[Operation.copy], Range], Dict[Literal[Operation.set_text], str]]]:
-# 	"""
-# 	This is not decode. It is interpretate or trans-something.
-#
-# 	"""
-# 	annotation, plain = tee(tape)
-# 	annotated_tape = zip(map(lambda p0t1: get_operation(p0t1[0], p0t1[1]), annotation), plain)
-#
-# 	annotated_tape = chain(
-# 		(head_tape,),
-# 		annotated_tape,
-# 	)
-#
-# 	last_text_path = None
-# 	previous_position = ((), 0, 0)
-# 	for operation, (pointer, text) in annotated_tape:
-# 		path = pointer.path
-# 		position = tuple(map(lambda n: getattr(pointer, n), ('path', 'offset', 'length')))
-# 		# debug(operation, position, make_x(tree_root, path), text)
-# 		# print('')
-# 		if operation is ElementOperation.none:
-# 			if copy_range_start:
-# 				root = get_root(copy_range_start, path)
-# 				branch_index = len(root)
-# 				start_successor = copy_range_start[branch_index] + 1
-# 				end_exclude = path[branch_index]
-# 				if end_exclude == start_successor:
-# 					yield {Operation.copy: Range(copy_range_start, root+(start_successor,))}
-# 				else:
-# 					yield {Operation.copy: Range(copy_range_start, root+(end_exclude-1,))}
-# 				copy_range_start = None
-#
-# 		elif operation is ElementOperation.copy:
-# 			if copy_range_start is None:
-# 				copy_range_start = path
-#
-# 		elif operation is ElementOperation.set_text:
-# 			if path != last_text_path:
-# 				yield {Operation.copy: Range(path, path)}
-# 				last_text_path = path
-# 			yield {Operation.set_text: text}
-#
-# 		else:
-# 			raise RuntimeError(f'Unsupported operation {operation}.', operation)
-#
-# 		assert previous_position != position, ((make_x(root, previous_position[0]), previous_position), (make_x(root, position[0]), position))
-# 		previous_position = position
-#
-#
-# def enforce(origin: 'FileLikeObject', tape: Iterator[Tuple[Element, str]], middleware) -> Tuple[ElementTree, list]:
-# 	"""
-# 	Enforce contains those operations:
-# 	- substitute variables
-# 	- skip terminals
-# 	- drop conditions body
-# 	- copy partial or allocate place for new iteration of cycle body
-# 	- mirror unchanged text in between
-#
-# 	Substitute requires managing text of element. Substitute can happen many times in one element or completely replace whole text.
-# 	Skip terminals is a like substitute with zero length text. It opens door to ignore some nodes from origin.
-# 	Drop conditions body requires accurate cut part of tree (partial copy in terms of this kind implementation) on boundary of different levels of hierarchy.
-# 	Vague operation of cycle body requires look behind on origin tree and construct new branch for inside operation from above.
-# 	Mirror unchanged text (in terms of template) is a suspect for variant of operations from above.
-# 	"""
-# 	# XXX: It is memory consumption here.
-# 	origin_tree = parse(origin)
-#
-# 	# XXX: It is just for starting implementation to copy root.
-# 	# XXX: It is bad design to copy each element from one tree to another.
-# 	result_tree = ElementTree(origin_tree.getroot().copy())
-# 	# Free tinking. Postpone ideas by finishing cycling.
-# 	# TreeBuilder is used to construct ElementTree instead of incremental dump as I think at fist.
-# 	# XXX: We don't use or rely of nodes in memory. Consider writing incremental dump against xml.etree.ElementTree._serialize_xml.
-# 	# XXX: We don't need parse origin neither. We copy substring from original to result.
-#
-# 	# XXX: return to Element and rewrite it for structuted path.
-# 	def restore_path(element: Element):
-# 		for (namespace_id, tag), index in element.path:
-# 			yield TagIndex(
-# 				namespace=element.namespaces[namespace_id],
-# 				name=tag,
-# 				index=index,
-# 			)
-#
-# 	log = []
-# 	origin_root = origin_tree.getroot()
-# 	result_root = element_copy(origin_root)
-# 	result = ElementTree(result_root)
-# 	element_tag = result_root.tag
-# 	namespace, tagname = '', element_tag
-# 	match = namespace_re.match(element_tag)
-# 	if match:
-# 		namespace, tagname = match.group('namespace', 'tagname')
-# 	destination = deque((result_root,))
-# 	source = deque((origin_root,))
-# 	initial_path = TagIndex(namespace, tagname, 0),
-# 	previous_path = initial_path
-# 	previous_offset = 0
-# 	for element, text in tape:
-# 		path = tuple(restore_path(element))
-# 		if path == previous_path and element.offset != previous_offset:
-# 			plane = Plane.text
-# 			direction = Direction.none
-# 			action = Action.none
-# 		else:
-# 			plane = Plane.element
-# 			if path > previous_path:
-# 				direction = Direction.forward
-# 			else:
-# 				direction = Direction.backward
-# 			action = Action.copy
-# 			if element.length and not text:
-# 				action = Action.skip
-#
-# 		tag_repr = lambda t: f'{t.name}[{t.index}]'
-# 		log.append([
-# 			['/'.join(map(tag_repr, previous_path)), '/'.join(map(tag_repr, path))],
-# 			[plane.name, direction.name, action.name],
-# 			text,
-# 		])
-# 		# debug(log[-1])
-#
-# 		if plane is Plane.element:
-# 			if action is Action.copy:
-# 				copy(source, destination, previous_path, path)
-# 				set(destination[-1], text)
-# 				previous_path = path
-# 				previous_offset = element.offset
-# 			elif action is Action.skip:
-# 				# explicit handling.
-# 				# XXX: call middleware for handling skipping. copy will be handling too in near future.
-# 				root_index = 0
-# 				for root_index, (a, b) in enumerate(zip(previous_path, path)):
-# 					if a != b:
-# 						root_index -= 1
-# 						break
-# 				source = deque(islice(source, root_index+1))
-# 				destination = deque(islice(destination, root_index+1))
-# 				previous_path = previous_path[:root_index+1]
-# 				previous_offset = element.offset
-# 			else:
-# 				raise RuntimeError('Unsupported action', action)
-# 		elif plane is Plane.text:
-# 			append(destination[-1], text)
-# 			previous_offset = element.offset
-# 		else:
-# 			raise RuntimeError('Unsupported plane', plane)
-#
-#
-# 	tag_repr = lambda t: f'{t.name}[{t.index}]'
-# 	log.append([
-# 		['/'.join(map(tag_repr, previous_path)), '/'.join(map(tag_repr, path))],
-# 		[plane.name, direction.name, action.name],
-# 		text,
-# 	])
-# 	# debug(log[-1])
-# 	copy(source, destination, previous_path, initial_path)
-#
-# 	return result, log
